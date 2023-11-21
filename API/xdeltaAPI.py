5ede17db7731ab5466ea8cc4a2f075396c5f83e8
silous888 <silous666@gmail.com>
2023-11-22 00:33:18 +0100

    add function to patch a file with xdelta patch, need to add error management

--- API/xdeltaAPI.py
+++ API/xdeltaAPI.py
@@ -1,11 +1,29 @@
 import subprocess as sp
 import os
+from time import sleep
+
 
 XDELTA_PATH = ""
+"""
+path where xdelta3.exe is, use "define_xdelta_path()" to change it<br>
+By default, the path is where you run your code
+"""
+
 XDELTA = "xdelta3"
+"""
+xdelta command in shell
+"""
 
 
-def define_xdelta_path(path):
+def define_xdelta_path(path) -> (int):
+    """redefine path for location of xdelta3.exe,
+
+    Args:
+        path (str): path of the folder where xdelta3.exe is
+
+    Returns:
+        int: 0 if xdelta3.exe has been found in this path, -1 otherwise, but the path will still be change
+    """
     global XDELTA_PATH
     if not path.endswith(os.path.sep):
         path = os.path.join(path, '')
@@ -15,18 +33,25 @@ def define_xdelta_path(path):
     return 0
 
 
-def create_patch(original_file, patched_file, name_patch_file="", patch_path="", overwrite=True):
+def create_patch(original_file, patched_file, name_patch_file="", patch_path="", overwrite=True) -> (int):
     """Create a xdelta3 patch file
 
     Args:
         file_no_patch (str): original file path
         file_patched (str): patched file path, so original file with modifications
-        name_patch_file (str, optional): patch file name without ".xdelta" part. Defaults name of file_no_patch.
+        name_patch_file (str, optional): patch file name without ".xdelta" part. Defaults, name of file_no_patch.
         path_patch (str, optional): path of patch file. Defaults to exec repertory.
         overwrite (bool, optional): overwrite or not patch file. Defaults to True.
 
     Returns:
-        _type_: _description_
+        int: 0 if the patch file is created, otherwise, return an error code.
+
+    error code:<br>
+    -1 if Xdelta3.exe is not found<br>
+    -2 if "original_file" is not found or is not a file<br>
+    -3 if "patched_file" is not found or is not a file<br>
+    -4 if the path in "patch_path" doesn't exist, or is not a folder<br>
+    -5 if overwrite is set to False, and the command can't overwrite an existing file
     """
     if XDELTA_PATH != "" and not os.path.exists(XDELTA_PATH):
         return -1
@@ -59,9 +84,33 @@ def create_patch(original_file, patched_file, name_patch_file="", patch_path="",
     return output.returncode
 
 
-def apply_patch():
+def apply_patch(file_to_patch, patch_file, name_patched_file="") -> (int):
     """by default, overwrite the file
     otherwise, specify the name of the patched file
     return error code if trying to patch a wrong file
     """
-    pass
+    def overwrite_original_file():
+        os.remove(file_to_patch)
+        sleep(0.3)
+        os.rename(name_patched_file, file_to_patch)
+
+    overwrite = False
+    if name_patched_file == "":
+        splitname = os.path.splitext(os.path.basename(file_to_patch))
+        name_patched_file = os.path.dirname(file_to_patch) + splitname[0] + "n" + splitname[1]
+        overwrite = True
+    command = [
+        XDELTA_PATH + XDELTA,
+        "-f",
+        "-d",  # uncompress
+        "-s",  # source
+        file_to_patch,
+        patch_file,
+        name_patched_file,
+    ]
+    process = sp.run(command, stdout=sp.PIPE, stderr=sp.PIPE,
+                     creationflags=sp.CREATE_NO_WINDOW)
+
+    if overwrite and process.returncode == 0:
+        overwrite_original_file()
+    return process.returncode
