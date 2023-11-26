3446b50b83c71e34f3717bf33769eac98194ba16
silous888 <silous666@gmail.com>
2023-11-26 17:34:13 +0100

    add function to download files in a Google drive folder recursively

--- API/googleDriveAPI.py
+++ API/googleDriveAPI.py
@@ -86,29 +86,36 @@ def download_file(file_id, local_folder=".\\"):
 
     Args:
         file_id (str): id of the file
-        local_folder (str, optional): path where the file will be. Defaults to ".\".
+        local_folder (str, optional): path where the file will be. Defaults to ".\\".
     """
-    def download_google_sheet_as_excel(file_metadata, local_folder):
+    def google_sheet_as_excel(file_metadata, local_folder):
         local_path = os.path.join(local_folder, file_metadata['name'] + '.xlsx')
         export_params = {'mimeType': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'}
         request = drive_service.files().export_media(fileId=file_metadata['id'], mimeType=export_params['mimeType'])
         return request, local_path
 
-    def download_google_doc_as_word(file_metadata, local_folder):
+    def google_doc_as_word(file_metadata, local_folder):
         local_path = os.path.join(local_folder, file_metadata['name'] + '.docx')
         export_params = {'mimeType': 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'}
         request = drive_service.files().export_media(fileId=file_metadata['id'], mimeType=export_params['mimeType'])
         return request, local_path
 
-    file_metadata = drive_service.files().get(fileId=file_id).execute()
+    ret = __init()
+    if ret != 0:
+        return ret
+
+    try:
+        file_metadata = drive_service.files().get(fileId=file_id).execute()
+    except Exception:
+        return -3
 
     if file_metadata['mimeType'].startswith('application/vnd.google-apps'):
         if 'application/vnd.google-apps.spreadsheet' in file_metadata['mimeType']:
-            request, local_path = download_google_sheet_as_excel(file_metadata, local_folder)
+            request, local_path = google_sheet_as_excel(file_metadata, local_folder)
         elif 'application/vnd.google-apps.document' in file_metadata['mimeType']:
-            request, local_path = download_google_doc_as_word(file_metadata, local_folder)
+            request, local_path = google_doc_as_word(file_metadata, local_folder)
         else:
-            return -1
+            return -4
     else:
         request = drive_service.files().get_media(fileId=file_id)
         file_name = file_metadata['name']
@@ -119,4 +126,28 @@ def download_file(file_id, local_folder=".\\"):
         done = False
         while not done:
             _, done = downloader.next_chunk()
+    return 0
+
+
+def download_files_in_folder(folder_id, local_folder=".\\", keep_folders=False):
+    ret = __init()
+    if ret != 0:
+        return ret
+
+    results = drive_service.files().list(
+        q=f"'{folder_id}' in parents and trashed=false",
+        fields='files(id, name, mimeType)'
+    ).execute()
+    files = results.get('files', [])
+    for file in files:
+        if file['mimeType'] == 'application/vnd.google-apps.folder':
+            if keep_folders:
+                new_local_folder = os.path.join(local_folder, file['name'])
+                if not os.path.exists(new_local_folder):
+                    os.makedirs(new_local_folder)
+            else:
+                new_local_folder = local_folder
+            download_files_in_folder(file['id'], new_local_folder, keep_folders)
+        else:
+            download_file(file['id'], local_folder)
     return 0
